c4e7290b0d8b8e4a6f0803cf7b85848ceeca81f8
hyuniiya <wngus7988.10@gmail.com>
2023-05-16 16:16:23 +0900

    불필요한 태그 삭제

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,17 @@
-const Main = () => {
-  return <div>Main</div>;
+
+
+import styled from "styled-components";
+
+
+export const MainContainer = styled.main`
+    width: 100%;
+    max-width: 92rem;
+    margin: 0 auto;
+    
+`;
+
+const Main = ({ products, isLoading }) => {
+    return <div>Main</div>
 };
 
 export default Main;
